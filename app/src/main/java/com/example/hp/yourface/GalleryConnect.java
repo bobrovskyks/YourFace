850bc5074b693fcd56801d5f43a95f746b7c030c
Konstantin_Bobrovsky_450501 <bobrovskyks@gmail.com>
2016-11-23 23:07:41 +0300

    Version 0.4.1

--- app/src/main/java/com/example/hp/yourface/GalleryConnect.java
+++
@@ -1,4 +0,0 @@
-package com.example.hp.yourface;
-
-public class GalleryConnect {
-}
--- app/src/main/java/com/example/hp/yourface/VersionApp.java
+++
@@ -1,8 +0,0 @@
-package com.example.hp.yourface;
-
-/**
- * Created by HP on 17.11.2016.
- */
-
-public interface VersionApp {
-}
